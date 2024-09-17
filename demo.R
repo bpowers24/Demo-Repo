36edf90f8e424f7942341c2ef66f47e26baaeccf
BP <66450457+bpowers24@users.noreply.github.com>
2024-09-17 13:31:43 -0700

    Update demo.R

--- demo.R
+++ demo.R
@@ -6,13 +6,13 @@ library(ggplot2)
 # create fake data
 set.seed(123)
 df <- data.frame(
-  ID = 1:10,
-  Category = rep(c("A", "B"), each = 5),
-  Value = sample(10:100, 10)
+  ID = 1:20,
+  Category = rep(c("A", "B", "D", "F"), each = 5),
+  Value = sample(10:100, 20)
 )
 
 # make plot
-ggplot(df, aes(x = factor(ID), y = Value, fill = Category)) +
+ggplot(df, aes(x = factor(ID), y = Value, fill = ID)) +
   geom_bar(stat = "identity", position = "dodge") +
   labs(
     title = "Value by ID and Category",
