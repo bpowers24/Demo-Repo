d85404e07119f3c489f061074f81737f188c43d9
BP <66450457+bpowers24@users.noreply.github.com>
2024-12-16 12:34:58 -0800

    answers for 12/17 workshop here!

--- Scripts/QAQC_zoo_animals.R
+++ Scripts/QAQC_zoo_animals.R
@@ -41,7 +41,10 @@ test_that("Data frames are equal", {
 
 # write your code here
 zoo_animals_processed <- zoo_animals_processed %>% 
-
+  left_join(weight_parameters, by = "species") %>% 
+  mutate(is_overweight = case_when(weight > max_weight ~ TRUE, 
+                                   TRUE ~ FALSE)) %>% 
+  select(colnames(zoo_animals_processed), is_overweight)
 
 
 # run this code to check your answer
@@ -57,7 +60,10 @@ test_that("Data frames are equal", {
 
 # write your code here
 zoo_animals_processed <- zoo_animals_processed %>% 
-
+  left_join(listing_status, by = "species") %>% 
+  mutate(is_threatened = case_when(iucn_listing_status %in% c("critically endangered", "endangered", "vulnerable") ~ TRUE, 
+                                   TRUE ~ FALSE)) %>% 
+  select(colnames(zoo_animals_processed), is_threatened)
 
 
 # run this code to check your answer
