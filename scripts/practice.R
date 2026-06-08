66c2182a1b7dcb999671137af7478afa483dd315
ryderrunk <ryder.run@gmail.com>
2026-06-08 16:03:14 -0700

    Join data sets together
    Joined the two datasets together to prepare to create island_body_mass

--- scripts/practice.R
+++ scripts/practice.R
@@ -2,3 +2,7 @@ print("hello")
 read_csv("data/penguin_species.csv")
 read_csv("data/penguin_measurements.csv")
 
+penguin_species = read_csv("data/penguin_species.csv")
+penguin_measurements = read_csv("data/penguin_measurements.csv")
+
+joined_data = left_join(penguin_measurements, penguin_species, by = "species_id")
