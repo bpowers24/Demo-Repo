7cd419b8c8dbd4e573c5f59507720c4aedcc4f1f
ryderrunk <ryder.run@gmail.com>
2026-06-09 12:06:24 -0700

    Finalize Code for Practice
    I finalize the code for the practice collab, and then I also added LINE 1 and LINE 2 make the code run smoother.

--- scripts/practice.R
+++ scripts/practice.R
@@ -1,3 +1,5 @@
+library("tidyverse")
+library("testthat")
 read_csv("data/penguin_species.csv")
 read_csv("data/penguin_measurements.csv")
 
@@ -9,5 +11,5 @@ joined_data = left_join(penguin_species, penguin_measurements, by = "species_id"
 adelie = filter(joined_data, common_name == "Adelie")
 
 island_body_mass = group_by(adelie, common_name, island)
-island_body_mass = summarize(island_body_mass, mean_body_mass_g = mean(body_mass_g, na.rm = TRUE, .groups = "Drop"))
-print(island_body_mass, digits = 10)
+island_body_mass = summarize(island_body_mass, mean_body_mass_g = mean(body_mass_g, na.rm = TRUE), .groups = "drop")
+print(island_body_mass, digits = 16)
