17b8c58e412b660e8b17519a357a96031c9a881a
ryderrunk <ryder.run@gmail.com>
2026-06-08 23:01:44 -0700

    Try to make final data frame
    I am making the current data frame, but am a little stuck (the last test keeps failing), so I am going to try new things

--- scripts/practice.R
+++ scripts/practice.R
@@ -4,7 +4,10 @@ read_csv("data/penguin_measurements.csv")
 penguin_species = read_csv("data/penguin_species.csv")
 penguin_measurements = read_csv("data/penguin_measurements.csv")
 
-joined_data = left_join(penguin_measurements, penguin_species, by = "species_id")
+joined_data = left_join(penguin_species, penguin_measurements, by = "species_id")
 
 adelie = filter(joined_data, common_name == "Adelie")
 
+island_body_mass = group_by(adelie, common_name, island)
+island_body_mass = summarize(island_body_mass, mean_body_mass_g = mean(body_mass_g, na.rm = TRUE, .groups = "Drop"))
+print(island_body_mass, digits = 10)
