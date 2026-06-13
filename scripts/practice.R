85314396d046900362c2c41d4da4b17b07a86a1a
ryderrunk <ryder.run@gmail.com>
2026-06-13 11:34:01 -0700

    Add comments throughout the file to describe what I'm doing doing each step
    I added descriptions of what I was doing between each step, so that it could clearly be identified what each step was for.

--- scripts/practice.R
+++ scripts/practice.R
@@ -1,15 +1,21 @@
 ## Ryder Runkle
 ## June, 9th, 2026
 
+## Load libraries necessary for the tasks
 library("tidyverse")
 library("testthat")
+
+## Create Data frames so to make csv data usable in R.
 penguin_species = read_csv("data/penguin_species.csv")
 penguin_measurements = read_csv("data/penguin_measurements.csv")
 
+## Joined Data frames so that I can later use species to identify average body masss. 
 joined_data = left_join(penguin_species, penguin_measurements, by = "species_id")
 
+## Filter for just the Species Adelie 
 adelie = filter(joined_data, common_name == "Adelie")
 
+## Create final data frame that shows the average body mass by islands for only the Adelie Species
 island_body_mass = group_by(adelie, common_name, island)
 island_body_mass = summarize(island_body_mass, mean_body_mass_g = mean(body_mass_g, na.rm = TRUE), .groups = "drop")
 print(island_body_mass, digits = 16)
