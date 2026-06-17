634fe10bc9f5d6e27bb18fb1c9e7e85c4cf15a86
ryderrunk <ryder.run@gmail.com>
2026-06-16 18:42:21 -0700

    Add Test for New Data Frame
    Added multiple tests to make sure that I create the adelie data frame correctly.

--- scripts/practice-test.R
+++ scripts/practice-test.R
@@ -56,3 +56,33 @@ test_that("averaged body mass values within `island_body_mass` are correct", {
               tolerance = 0.01, 
             label = "The averaged body mass values within the `mean_body_mass_g` column of the `island_body_mass` object are not correct. Please check that you have created an object with this name in your `practice.R` script and that it contains the correct averaged body mass values for each island.")
 })
+
+# check that object `adelie_body_mass` exsits 
+test_that("object `adelie_body_mass` exists", {
+  expect_true(exists("adelie_body_mass"), 
+              label = "The object `adelie_body_mass` does not exist. Please check that you have created an object with this name in your `practice.R` script.")
+})
+
+# check that only Adelie species is included in data set
+test_that("only Adelie is included in adelie_body_mass", {
+  expect_true(all(adelie_body_mass$common_name %in% "Adelie"), 
+              label = "The object `adelie_body_mass` contains penguins that are not Adelie penguins.")
+})
+
+# check that there are no missing values in the `mean_body_mass_g` column within `adelie_body_mass`
+test_that("no missing values in output", {
+  expect_false(any(is.na(adelie_body_mass$mean_body_mass_g)), 
+               label = "The object `adelie_body_mass` contains missing values in the `mean_body_mass_g` column. Please check that you have created an object with this name in your `practice.R` script and that it contains no missing values.")
+})
+
+# check that averaged body mass values within `island_body_mass` are correct
+test_that("averaged body mass value within `adelie_body_mass` is correct", {
+  expected = tibble(common_name = c("Adelie"),
+                    mean_body_mass_g = c(3700.662))
+  
+  expect_equal(adelie_body_mass %>% 
+                 select(common_name, mean_body_mass_g),
+               expected,
+               tolerance = 0.01, 
+               label = "The averaged body mass value within `adelie_body_mass` is not correct.")
+})
