9d6a4a70af58512520f7f546e87e751901ea9072
ryderrunk <ryder.run@gmail.com>
2026-06-22 18:22:41 -0700

    Create Data Frame
    created the data frame that I can use for the rest of the tasks

--- analyses:exploratory_plots/exploratory_plots.R
+++ analyses:exploratory_plots/exploratory_plots.R
@@ -8,3 +8,7 @@ library("patchwork")
 library("ggcorrplot")
 library("dplyr")
 
+# Create Data Frame to use for rest of tasks
+penguins = read.csv("data/penguin_measurements.csv") %>%
+  left_join(read.csv("data/penguin_species.csv"), by = "species_id")
+
