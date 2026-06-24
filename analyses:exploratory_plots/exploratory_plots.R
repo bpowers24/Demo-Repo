758ab8d42c39c5cb09e9d081af70db3644ee251c
ryderrunk <ryder.run@gmail.com>
2026-06-24 12:53:22 -0700

    Use Patch Work
    Used the patchwork function to combine the violin plot and the bar plot

--- analyses:exploratory_plots/exploratory_plots.R
+++ analyses:exploratory_plots/exploratory_plots.R
@@ -8,24 +8,24 @@ library("patchwork")
 library("ggcorrplot")
 library("dplyr")
 
-# Create Data Frame to use for rest of tasks
+# Create data drame to use for rest of tasks
 penguins = read.csv("data/penguin_measurements.csv") %>%
   left_join(read.csv("data/penguin_species.csv"), by = "species_id")
 
-# Create Point Plot
+# Create point plot
 pointplot = ggplot(data = penguins, mapping = aes(x = bill_length_mm, y = body_mass_g)) +
   geom_point(size = 2, color = "red") +
   labs( x = "Bill Length (mm)", y = "Body Mass (g)",
        title = "Body Mass Based on Bill Length")
 
-# Create Bar Plot
+# Create bar plot
 barplot = ggplot(data = penguins, mapping = aes(x = island, fill = common_name)) +
   geom_bar() +
   scale_fill_manual(values = c("red", "yellow", "blue")) + 
   labs(x = "Name of Island", y = "Species",
        title = "Species by Island")
 
-# Create Plot using `facet_wrap`
+# Create plot using `facet_wrap`
 flipperplot = ggplot(data = penguins, mapping = aes(x = sex, y = flipper_length_mm, color = sex)) +
   geom_point(size = 2) +
   scale_color_manual(values = c("female" = "pink", "male" = "blue")) +
@@ -33,10 +33,13 @@ flipperplot = ggplot(data = penguins, mapping = aes(x = sex, y = flipper_length_
   labs(x = "Sex", y = "Flipper Length",
        title = "Flipper Length by Sex by Island")
 
-# Explore other Geom Plots
+# Explore other geom plots
 bodymassplot = ggplot(data = penguins, mapping = aes(x = common_name, y = body_mass_g, fill = common_name)) +
   geom_violin() +
   scale_fill_manual(values = c("red", "yellow", "blue")) + 
   labs(x = "Species", y = "Body Mass (g)",
        title = "Body Mass Distribution by Species")
 
+# Use Patch work to combine two plots
+patchplot = barplot + bodymassplot & 
+  theme_minimal()
